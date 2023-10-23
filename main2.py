06edd6b48fc2f77839dd500f1e99b5833bd4e077
Harshavardhan Bajoria <62978274+HVbajoria@users.noreply.github.com>
2023-10-23 12:01:09 +0530

    Added Footer

--- main2.py
+++ main2.py
@@ -92,3 +92,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+footer="""<style>
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤️ by <a style='display: inline; text-align: center;' href="https://www.linkedin.com/in/harshavardhan-bajoria/" target="_blank">Harshavardhan Bajoria</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
